379e2213366988ec738f88d145b9cd4e67f17789
Pylypchuk Valentyn <pylypchuk.vlntn@gmail.com>
2020-10-28 05:37:46 +0200

    Added QuizList main functionality and styles

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,18 +1,35 @@
 import React, {Component} from "react";
 import classes from "./Drawer.css";
 import Backdrop from "../../UI/Backdrop/Backdrop";
+import {NavLink} from "react-router-dom";
 
 const links = [
-    1,2,3
+    {to: "/", label: "List", exact: true},
+    {to: "/auth", label: "Authorize", exact: false},
+    {to: "/quiz-creator", label: "Create quiz", exact: false},
+    // {to: "/", label: "List", exact: true},
 ]
 
+
 class Drawer extends Component {
+
+
+    clickHandler = () => {
+        this.props.onClose();
+    }
 
     renderLinks() {
         return links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a>Link {link}</a>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={classes.active}
+                        onClick={this.clickHandler}
+                    >
+                        {link.label}
+                    </NavLink>
                 </li>
             )
         })
