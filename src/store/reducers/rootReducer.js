099e72d2ce18162db56ef44e0ea91f2876497e88
Pylypchuk Valentyn <pylypchuk.vlntn@gmail.com>
2020-11-24 12:24:51 +0200

    QuizCreator modified with redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import quizReducer from "../reducers/quiz";
+import createReducer from "../reducers/create";
 
 export default combineReducers({
-    quiz: quizReducer
+    quiz: quizReducer,
+    create: createReducer
 })
